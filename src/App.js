ba498ef3a26ea3ce82c43c6320706d8d8c33cded
vitamin777 <rlaalsdn8@gmail.com>
2022-07-30 23:20:57 +0900

    perf : prop chain 을 context 활용으로 개선
    - store 폴더 추가 및 하위 파일 auth-context.js 추가
    - AuthContext 로 wrapper 활용 및 isLoggedIn 전역 활용 기능 개선
    - isLoggedIn 의 true, false 에 따라 네비게이터 노출 및 비노출 적용
    - 적용 및 기능 정상 작동 확인 커밋

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from "./Store/auth-context";
 
 function App() {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -27,13 +28,18 @@ function App() {
 	};
 
 	return (
-			<React.Fragment>
-				<MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler}/>
+			/*AuthContext 를 Wrapper 로 사용(root 수준 컴포넌트로 반환하기때문)
+			* MainHeader 뿐만 아니라 Login 및 Home 컴포넌트 등 모든 자식은 AuthContext 에 접근할 수 있음
+			* */
+			<AuthContext.Provider value={{ /*객체 전달 가능하며 해당 객체를 변경 할 수 있다 ex) state 나 앱컴포넌트를 통해 변경경될때마다 새 값이 모든 소비 컴포넌트에 전달됨*/
+				isLoggedIn: isLoggedIn, /*value 객체는 isLoggedIn 이 변경 될때마다 리액트에의해 업데이트됨 그리고 새로운 객체 새로운 컨텍스트 객체는 모든 리스닝 컴포넌트로 전달됨*/
+			}}>
+				<MainHeader onLogout={logoutHandler}/>
 				<main>
 					{!isLoggedIn && <Login onLogin={loginHandler}/>}
 					{isLoggedIn && <Home onLogout={logoutHandler}/>}
 				</main>
-			</React.Fragment>
+			</AuthContext.Provider>
 	);
 }
 
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,29 +1,35 @@
 import React from 'react';
 
 import classes from './Navigation.module.css';
+import AuthContext from '../../Store/auth-context'
 
 const Navigation = (props) => {
-  return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
-  );
+	return (
+			/*소비자는 자식을 가지고 함수여야함*/
+			<AuthContext.Consumer>
+				{(contextLoggedIn) => {
+					return <nav className={classes.nav}>
+						<ul>
+							{contextLoggedIn.isLoggedIn && (
+									<li>
+										<a href="/">Users</a>
+									</li>
+							)}
+							{contextLoggedIn.isLoggedIn && (
+									<li>
+										<a href="/">Admin</a>
+									</li>
+							)}
+							{contextLoggedIn.isLoggedIn && (
+									<li>
+										<button onClick={props.onLogout}>Logout</button>
+									</li>
+							)}
+						</ul>
+					</nav>
+				}}
+			</AuthContext.Consumer>
+	);
 };
 
 export default Navigation;
