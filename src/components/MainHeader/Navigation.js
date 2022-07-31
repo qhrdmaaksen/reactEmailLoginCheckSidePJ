c7007a4fac3d93ea095b713aa4fcc53d7bcf3cce
vitamin777 <rlaalsdn8@gmail.com>
2022-07-31 16:23:51 +0900

    refactor : consumer context 를 useContext Hook 전환
    - 간결한 코드를 위한 변환
    - 적용 및 기능 정상 작동 확인 커밋

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,34 +1,31 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from '../../Store/auth-context'
 
 const Navigation = (props) => {
+	const contextLoggedIn = useContext(AuthContext)
 	return (
 			/*소비자는 자식을 가지고 함수여야함*/
-			<AuthContext.Consumer>
-				{(contextLoggedIn) => {
-					return <nav className={classes.nav}>
-						<ul>
-							{contextLoggedIn.isLoggedIn && (
-									<li>
-										<a href="/">Users</a>
-									</li>
-							)}
-							{contextLoggedIn.isLoggedIn && (
-									<li>
-										<a href="/">Admin</a>
-									</li>
-							)}
-							{contextLoggedIn.isLoggedIn && (
-									<li>
-										<button onClick={props.onLogout}>Logout</button>
-									</li>
-							)}
-						</ul>
-					</nav>
-				}}
-			</AuthContext.Consumer>
+			<nav className={classes.nav}>
+				<ul>
+					{contextLoggedIn.isLoggedIn && (
+							<li>
+								<a href="/">Users</a>
+							</li>
+					)}
+					{contextLoggedIn.isLoggedIn && (
+							<li>
+								<a href="/">Admin</a>
+							</li>
+					)}
+					{contextLoggedIn.isLoggedIn && (
+							<li>
+								<button onClick={props.onLogout}>Logout</button>
+							</li>
+					)}
+				</ul>
+			</nav>
 	);
 };
 
